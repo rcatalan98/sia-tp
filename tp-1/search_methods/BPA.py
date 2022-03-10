a869d04dc49d739e8bbde87fc1c428c4746aa30e
Lucas Dell'Isola <dev@lucasdellisola.com.ar>
2022-03-10 16:12:31 -0300

    fixed BPA

--- tp-1/search_methods/BPA.py
+++ tp-1/search_methods/BPA.py
@@ -1,4 +1,4 @@
-from typing import Tuple, List
+from typing import Tuple, List, Set
 
 from models import Node, Solution
 from search_methods.Base import Base
@@ -10,20 +10,15 @@ class BPA(Base):
         # It's a tuple that stores the node and the depth
         frontier_nodes: List[Tuple[int, Node]] = list([(0, root)])
         explored_nodes: List[Node] = list()
-
-
-
-
-
-
-
-
+        known_states: Set[Node] = set()
 
         while len(frontier_nodes) != 0:
             element = frontier_nodes.pop(0)
             depth = element[0]
             node = element[1]
             children: List[Node] = node.get_children()
+            known_states.add(node.state)
+            known_states.union(map(lambda s: s.state,children))
 
             # See if it is the goal
 
@@ -34,12 +29,10 @@ class BPA(Base):
             explored_nodes.append(node)
 
             nodes_to_add = filter(
-                lambda n: (n not in explored_nodes) and (n not in frontier_nodes),
+                lambda n:  n.state not in known_states,
                 children
             )
 
             frontier_nodes.extend(list(map(lambda x: (depth + 1, x), nodes_to_add)))
-
-            print(frontier_nodes)
 
         return False
