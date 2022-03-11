66b6c0f9c69a199d2b894299e8fd7fe30b6d9661
lgalende <lgalende@itba.edu.ar>
2022-03-11 15:42:35 -0300

    AStar method

--- tp-1/heuristics/Base.py
+++ tp-1/heuristics/Base.py
@@ -1,7 +1,11 @@
 from models import State
 
 
+# TODO
 class Base:
+
+    def __init__(self, heuristic):
+        self.h = heuristic
 
     def get_value(self, state: State) -> int:
         raise "Method Not Implemented"
--- tp-1/search_methods/AStar.py
+++ tp-1/search_methods/AStar.py
@@ -1,13 +1,35 @@
 from models import Node, Solution
 from search_methods.Base import Base
 from heuristics.Base import Base as Heuristic
+from typing import Tuple, List, Set
 
 
 class AStar(Base):
 
-    def __init__(self, heuristic: Heuristic):
-        self.heuristic = heuristic
+    def __init__(self, heuristic):
+        self.heuristic = Heuristic(heuristic)  # FIXME: What else?
 
     def search(self, root: Node) -> Solution:
-        return super().search(root)
+        frontier_nodes: List[Tuple[int, Node]] = list([(0, root)])
+        explored_nodes: Set[Node] = set()  # FIXME: Check if known_states is also needed
+        # TODO: solution = Solution(...)
 
+        while frontier_nodes:
+            element = frontier_nodes.pop(0)
+            depth = element[0]
+            node = element[1]
+
+            if node.state.is_solved():
+                return node  # TODO return solution
+
+            explored_nodes.add(node)
+            children: List[Node] = node.get_children()
+            frontier_nodes.extend(list(map(lambda x: (depth + 1, x), children)))
+
+            # Reorder F according to f(n) value
+            frontier_nodes.sort(key=self.reorder_by_fn)
+
+        return False  # TODO return solution
+
+    def reorder_by_fn(self, n: Tuple[int, Node]):
+        return n[0] + self.heuristic.h(n[1])  # g(n) + h(n.s)
