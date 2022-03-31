01360baabe22c18ed67ae456aa4077d4015c83f1
lgalende <lgalende@itba.edu.ar>
2022-03-31 10:55:55 -0300

    PoolManager.py refactor

--- tp-2/PoolManager.py
+++ tp-2/PoolManager.py
@@ -9,8 +9,7 @@ class PoolManager:
     def __init__(self, config: ConfigStore):
         self.population: List[Bag] = self.create_random_population()
         self.mutator: BaseMutator = config.get_mutator()
-        self.first_selector = config.get_first_selector()
-        self.second_selector = config.get_second_selector()
+        self.selector = config.get_selector()
         self.breeder = config.get_breeder()
         self.generation = 0
 
@@ -19,9 +18,9 @@ class PoolManager:
 
     def get_new_generation(self) -> List[Bag]:
         couples = self.first_selector(self.population, 2)
-        children: List[Bag] = List[Bag]()
+        children: List[Bag] = []
         for a, b in couples:
-            children.extend(map(self.mutator(), self.breeder(a, b)))
+            children.extend([self.mutator.mutate(el) for el in self.breeder.breed(a, b)])
         new_generation = self.second_selector(self.population+children)
         self.generation += 1
         return new_generation
