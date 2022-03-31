13e3910690058b15b5816f97d6e64c73934765fd
Roberto J Catalan <rcatalan@itba.edu.ar>
2022-03-31 11:33:09 -0300

    RankSelection refactor. Should work as intended.

--- tp-2/Selection/RankSelection.py
+++ tp-2/Selection/RankSelection.py
@@ -5,14 +5,17 @@ from Bag import Bag
 from Selection.BaseSelection import BaseSelection
 
 
-def get_rank_fitness(bag: Bag, population_size: int) -> int:
-    return (population_size - 1 - bag.fitness) / population_size
+def get_rank_fitness(rank: int, population_size: int) -> int:
+    return (population_size - 1 - rank) / population_size
 
+def rank(bag: Bag)->int:
+    return bag.fitness
 
 class RankSelection(BaseSelection):
 
     def select(self, population: List[Bag], population_size: int) -> List[Bag]:
-        final_bags = random.choices(population, weights=[get_rank_fitness(p.fitness, population_size)
-                                                         for p in population],
+        population.sort(key=rank)
+        final_bags = random.choices(population, weights=[get_rank_fitness(i, population_size)
+                                                         for i, p in enumerate(population)],
                                     k=population_size)
         return final_bags
