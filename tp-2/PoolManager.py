06d2044d9ed969996edeaf48b192425c702129f9
lgalende <lgalende@itba.edu.ar>
2022-03-31 10:58:59 -0300

    PoolManager.py refactor

--- tp-2/PoolManager.py
+++ tp-2/PoolManager.py
@@ -17,11 +17,11 @@ class PoolManager:
         pass
 
     def get_new_generation(self) -> List[Bag]:
-        couples = self.first_selector(self.population, 2)
+        couples = self.selector(self.population, 2)  # ToDo: select couples randomly
         children: List[Bag] = []
         for a, b in couples:
             children.extend([self.mutator.mutate(el) for el in self.breeder.breed(a, b)])
-        new_generation = self.second_selector(self.population+children)
+        new_generation = self.selector(self.population+children)
         self.generation += 1
         return new_generation
         # ToDo cuando y donde nos fijamos el corte y cuando pisar la poblacion
