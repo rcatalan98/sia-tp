d615df43d591bfa8000e31d23bdc8a754044ecac
lgalende <lgalende@itba.edu.ar>
2022-03-31 11:35:03 -0300

    mutate random number fix

--- tp-2/Mutation/Mutation.py
+++ tp-2/Mutation/Mutation.py
@@ -1,13 +1,14 @@
 from Bag import Bag
 import random
 
+
 class Mutation:
 
     def mutate(self, subject: Bag, p: float) -> Bag:
         mutated_bag: Bag = Bag()
         for i in range(0, len(subject.items)):
             item = subject.items[i]
-            u = random.randint(0, 1)
+            u: float = random.uniform(0, 1)
             mutated_bag.items[i] = not item if p < u else item
         return mutated_bag
 
