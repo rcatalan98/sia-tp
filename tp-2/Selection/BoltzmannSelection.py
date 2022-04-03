6ea945715d28c02daee672caaa5ced2c1c165248
Roberto J Catalan <rcatalan@itba.edu.ar>
2022-04-03 17:53:14 -0300

    Bugfix boltzmann

--- tp-2/Selection/BoltzmannSelection.py
+++ tp-2/Selection/BoltzmannSelection.py
@@ -8,13 +8,20 @@ from Selection.BaseSelection import BaseSelection
 
 class BoltzmannSelection(BaseSelection):
 
-    def __init__(self, k: float, t0: float, tc: float, n_generation: int):
-        self.temperature: float = tc + (t0 - tc) * math.exp(-k * n_generation)
+    def __init__(self, k: float, t0: float, tc: float):
+        self.k = k
+        self.t0 = t0
+        self.tc = tc
+        self.current_gen = 0
+
+    def temperature(self, n_generation: int) -> float:
+        return self.tc + (self.t0 - self.tc) * math.exp(-self.k * n_generation)
 
     def boltz_fitness(self, bag: Bag):
-        return math.exp(bag.fitness/self.temperature)
+        return math.exp(bag.fitness/self.temperature(self.current_gen))
 
     def select(self, population: List[Bag]) -> List[Bag]:
+        self.current_gen += 1
         return random.choices(
             population,
             weights=[self.boltz_fitness(bag) for bag in population],
