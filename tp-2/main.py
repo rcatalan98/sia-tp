1cc90b40034fe9e2aa53a91a6900fcfd6164294f
Lucas Dell'Isola <dev@lucasdellisola.com.ar>
2022-04-04 15:46:40 -0300

    Updated parameters

--- tp-2/main.py
+++ tp-2/main.py
@@ -2,7 +2,7 @@ import json
 import os
 import sys
 from time import perf_counter
-from typing import List
+from typing import List, Dict
 
 from ConfigStore import ConfigStore
 from PoolManager import PoolManager
@@ -32,10 +32,17 @@ def load_settings(item_description_filename: str, config_filename: str) -> Confi
 
     return ConfigStore(max_weight=max_weight, max_elements=max_elements, items=items, **config_data)
 
+
 def load_multiple_settings(item_description_filename: str, config_directory: str) -> List[ConfigStore]:
-    a = [load_settings(item_description_filename,val) for sublist in [[os.path.join(i[0], j) for j in i[2]] for i in os.walk(config_directory)] for val in sublist]
+    a = [load_settings(item_description_filename, val) for sublist in
+         [[os.path.join(i[0], j) for j in i[2]] for i in os.walk(config_directory)] for val in sublist]
 
     return a
+
+
+def format_dict(d: Dict):
+    return ' | '.join([f"{a[0]}: {a[1]}" for a in d.items()])
+
 
 def run(config: ConfigStore) -> str:
     start_time = perf_counter()
@@ -44,18 +51,18 @@ def run(config: ConfigStore) -> str:
     # TODO: Por que necesitamos guardar todas las generaciones???
     # all_generations: List[List[Bag]] = [pool_manager.generation]
 
-
     while not pool_manager.has_reached_stop_condition():
         next_gen: List[Bag] = pool_manager.get_new_generation()
-        # print(f"{pool_manager.generation}: {pool_manager.all_fitness[-1]}")
+        print(f"{pool_manager.generation}: {pool_manager.all_fitness[-1]}")
 
     end_time = perf_counter()
-    return f"{config.breeder},{config.breeding_arguments}," \
-           f"{config.selection},{config.selection_arguments}," \
-           f"{config.stop_condition}, {config.stop_condition_config}," \
+    return f"{config.breeder},{format_dict(config.breeding_arguments)}," \
+           f"{config.selection},{format_dict(config.selection_arguments)}," \
+           f"{config.stop_condition}, {format_dict(config.stop_condition_config)}," \
            f"{config.population_size},{config.mutation_rate}," \
            f"{pool_manager.all_weights[-1]},{pool_manager.all_fitness[-1]},{pool_manager.generation}," \
            f"[{';'.join(str(i) for i in pool_manager.all_fitness)}]"
+
 
 if __name__ == '__main__':
     if len(sys.argv) < 3:
@@ -64,29 +71,27 @@ if __name__ == '__main__':
     settings: List[ConfigStore] = []
     runs: int = 1
     if sys.argv[2] == 'multiple':
-        settings = load_multiple_settings(sys.argv[1],sys.argv[3])
+        settings = load_multiple_settings(sys.argv[1], sys.argv[3])
         if len(sys.argv) == 5:
             runs = int(sys.argv[4])
     else:
-        settings.append(load_settings(sys.argv[1],sys.argv[2]))
+        settings.append(load_settings(sys.argv[1], sys.argv[2]))
         if len(sys.argv) == 4:
             runs = int(sys.argv[3])
 
-
-
-    report: str = "breeder, breeder args, selector, selector_args," \
-                  "stop condition,stop condition args, population size," \
-                  "mutation rate, final weight, final fitness, max generation," \
-                  "fitness per generation\n"
-
     for config in settings:
+        report: List[str] = [
+            "breeder, breeder args, selector, selector_args,"
+            "stop condition,stop condition args, population size,"
+            "mutation rate, final weight, final fitness, max generation,"
+            "fitness per generation"
+        ]
         for i in range(runs):
             print(config.stop_condition)
             a = run(config)
             print(a)
-            report += f"{a}\n"
+            report.append(a)
 
-    f = open("results.csv",'w')
-    f.write(report)
-    f.close()
-
+        filename: str = f"{config.breeder} {config.selection} {config.stop_condition}.csv"
+        with open(filename, 'w') as file:
+            file.writelines(report)
