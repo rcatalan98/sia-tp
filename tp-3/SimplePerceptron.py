c72235fc188fd46be29da645b5ad01700a10de0b
Lucas Dell'Isola <dev@lucasdellisola.com.ar>
2022-04-19 11:41:45 -0300

    fixed perceptron for and but it's not working for XOR

--- tp-3/SimplePerceptron.py
+++ tp-3/SimplePerceptron.py
@@ -10,38 +10,47 @@ class SimplePerceptron:
     # Calcula la funcion w_1 *x_1 + w_2 * x_2 ...
     @staticmethod
     def excitement(w, datapoint):
-        return np.dot(w, datapoint) #esto no calcula lo que queremos!
+        return np.dot(datapoint,w) #esto no calcula lo que queremos!
 
     @staticmethod
     def activation(excitement_value):
-        return 1 if excitement_value else -1
+        return 1 if excitement_value >= 0 else -1
 
     def estimate_error(self, train_dataset, expected_results, w):
         results = np.array([self.activation(self.excitement(w, x)) for x in train_dataset])
         misclassified = np.count_nonzero(results != expected_results)
         return float(misclassified / len(expected_results))
 
+    def augment_dataset(self, train_dataset, position=2 ,value = 1):
+        return np.insert(train_dataset, position, value, axis=1)
+
     def train(self, train_dataset, expected_results, max_iteration, learning_rate):
-        error = 1
         p = len(expected_results)
+        error = 1
         error_min = 2 * p
-        w = np.random.random(2) * 2 - 1
+        train_dataset = self.augment_dataset(train_dataset)
+        w = np.zeros(3)
         self.w = np.copy(w)
-
-        for it in range(max_iteration):
-            print(f'Iteration number:{it}')
-            if error == 0:
-                break
-
-            i_x = random.randint(0, p-1)
-            h = self.excitement(w, train_dataset[i_x])
+        it = 0
+        while error > 0 and it < max_iteration:
+            # print(f'Iteration number:{it}, error: {error}, w: {w}')
+            i_x = random.randint(0, p - 1)
+            h = self.excitement(w,train_dataset[i_x])
             o = self.activation(h)
-            delta_w = np.dot(learning_rate * (expected_results[i_x] - o), train_dataset[i_x])
-            w = np.add(w, delta_w)
+            delta_w = learning_rate * np.dot(expected_results[i_x] - o, train_dataset[i_x])
+            w = np.add(w,delta_w)
             error = self.estimate_error(train_dataset, expected_results, w)
-
             if error < error_min:
                 error_min = error
                 self.w = np.copy(w)
+            else:
+                w = np.copy(self.w)
+
+            it += 1
+
+        print(f'Iteration number:{it}, error: {error}, w: {w}')
 
         return self.w
+
+    def perform(self,value):
+        return self.activation(self.excitement(self.w,self.augment_dataset(np.array([value]))))
--- tp-3/main.py
+++ tp-3/main.py
@@ -14,9 +14,22 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    data_set = np.array([[-1, 1], [1, -1], [-1, -1], [1, 1]])
-    expected_results = np.array([-1, -1, -1, 1])
-    perceptron = SimplePerceptron()
-    w = perceptron.train(data_set, expected_results, 100, 0.2)
-    print(f'the best w: {w}')
+    and_data_set = np.array([[-1, 1], [1, -1], [-1, -1], [1, 1]])
+    and_expected_results = np.array([-1, -1, -1, 1])
+    perceptron_and = SimplePerceptron()
+    perceptron_and.train(and_data_set, and_expected_results, 100, 0.2)
+
+    for val in and_data_set:
+        print(f"\t{val[0]} \tand \t{val[1]} \tis \t{perceptron_and.perform(val)}")
+
+    xor_data_set = np.array([[-1,1],[1,-1],[-1,-1],[1,1]])
+    xor_expected_results = np.array([1,1,-1,-1])
+    perceptron_xor = SimplePerceptron()
+    perceptron_xor.train(xor_data_set,xor_expected_results,100,0.9)
+
+    for val in xor_data_set:
+        print(f"\t{val[0]} \tand \t{val[1]} \tis \t{perceptron_xor.perform(val)}")
+
+
+    # print(f'the best w: {w}')
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
