Fixed backpropagation

tp-3/NeuralNetwork.py
@@ -48,13 +48,13 @@ class NeuralNetwork:
         self.w = []
 
         for (prev, next) in d:
-            self.w.append(np.zeros((next[0],prev[0]))) #np.random.uniform(-1.0, 1.0, (next[0], prev[0])))
+            self.w.append(np.random.uniform(-1.0, 1.0, (next[0], prev[0])))
 
         self.bias = []
         self.activation_functions = []
         self.derived_functions = []
         for layer in layers:
-            self.bias.append(np.ones((layer[0], 1)))
+            self.bias.append(np.random.uniform(-1.0, 1.0, (layer[0], 1)))
             self.activation_functions.append(np.vectorize(layer[1]))
             self.derived_functions.append(np.vectorize(layer[2]))
 
@@ -140,7 +140,7 @@ class NeuralNetwork:
             entry = output
 
         final_output = output_values[-1]
-        output_error = result - final_output
+        output_error = result.reshape((result.size,1)) - final_output
 
         # Backpropagation
 
@@ -155,13 +155,18 @@ class NeuralNetwork:
         # El gradiente tiene que ser un numero o una matriz??
         for i in reversed(range(self.layer_count)):
             gradient = self.derived_functions[i](excitement_values[i])
-            gradient = np.dot(gradient.T, errors[i])
+            gradient = gradient * errors[i]
             gradient = gradient * learning_rate
             self.bias[i] = self.bias[i] + gradient
-            delta_w = np.dot(input_for_each_layer[i],gradient)
-            self.w[i] += delta_w.T
+            delta_w = np.dot(gradient, input_for_each_layer[i].T)
+            self.w[i] += delta_w
 
         return self.w
+
+#Metemos durante crupto? o que hacemos?
+    # yo diria que comamos y lo veamos antes que cripto
+    # despues quiero empezar a meterle a esa materia
+    # otra cosa, aca
 
 
     # def __init__(self, input_nodes: int, hidden_nodes: int, output_nodes: int, learning_rate: float, bias: int = 1,

tp-3/main.py
@@ -52,21 +52,22 @@ if __name__ == '__main__':
     sigmoid = lambda e: 1 / (1 + math.exp(-e))
     sigmoid_derived = lambda e: sigmoid(e) * (1 - sigmoid(e))
 
-    nn = NNBuilder\
-                    .with_input(2)\
-                    .with_hidden_layer(2,sigmoid, sigmoid_derived)\
-                    .with_output_layer(1,sigmoid, sigmoid_derived)
+
 
 
     # a = nn.train([1,0],1,0.1)
 
     nn2 = NNBuilder\
                     .with_input(2)\
-                    .with_hidden_layer(2,sigmoid, sigmoid_derived)\
-                    .with_output_layer(2,sigmoid, sigmoid_derived)
+                    .with_hidden_layer(4,sigmoid, sigmoid_derived)\
+                    .with_output_layer(3,sigmoid, sigmoid_derived)
 
-    a2 = nn2.train2([1,0],[1,1],0.1)
+    a2 = nn2.train2([0,0],[1,0,0],0.1)
 
+    # Ahi CREO que funciona el backtracking. Al menos no explota.
+    # No se si el gradiente deberia ser un escalar, una matriz o un vector
+    # falta tocar el tema de los biases, lo tengo hecho a medias pero tengo que terminar el
+    # video del chabon
 
     xor_data_set = np.array([[0, 1], [1, 0], [0, 0], [1, 1]])
     xor_expected_results = np.array([1, 1, 0, 0])