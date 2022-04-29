3d76e714487ab8bca1d0b8513eb78a3bd60b0f05
lgalende <lgalende@itba.edu.ar>
2022-04-29 13:41:56 -0300

    ej3 functions now return training and testing errors

--- tp-3/Ej3.py
+++ tp-3/Ej3.py
@@ -3,7 +3,7 @@ import random
 import numpy as np
 
 from NeuralNetwork import NNBuilder
-from Utils import flatten_array, shuffle_data
+from Utils import flatten_array, shuffle_data, avg
 
 sigmoid = lambda e: 1 / (1 + math.exp(-e))
 sigmoid_derived = lambda e: sigmoid(e) * (1 - sigmoid(e))
@@ -24,7 +24,7 @@ def MultilayerPerceptronXor(hidden_layer_nodes: int = 5, iterations=600, epochs=
     for val in xor_data_set:
         print(f"\t{val[0]} \txor \t{val[1]} \tis \t{round(nn.feed_forward(val)[0])}")
 
-    return errors
+    return errors[-1], nn.get_error_on_dataset(xor_data_set, xor_expected_results)
 
 
 def MultilayerPerceptronMnistEvenOrOdd(hidden_layer_nodes: int = 30, iterations=100, epochs=30, learning_rate=0.1):
@@ -54,11 +54,15 @@ def MultilayerPerceptronMnistEvenOrOdd(hidden_layer_nodes: int = 30, iterations=
         test_input = dataset[9:]
         test_output = result[9:]
 
-        nn.train_on_dataset(training_input, training_output, iterations, epochs, learning_rate)
+        training_errors = nn.train_on_dataset(training_input, training_output, iterations, epochs, learning_rate)
 
         for i in range(len(test_output)):
             print(test_input[i].reshape(7, 5))
             print(f"expected; {test_output[i]}, got: {nn.feed_forward(test_input[i])}")
+
+        testing_error = nn.get_error_on_dataset(test_input, test_output)
+
+        return avg(training_errors[-1]), avg(testing_error)
 
 
 def MultilayerPerceptronMnistRecognizeNumber(probability, hidden_layer_nodes: int = 30, iterations=100, epochs=30, learning_rate=0.1):
@@ -78,7 +82,7 @@ def MultilayerPerceptronMnistRecognizeNumber(probability, hidden_layer_nodes: in
         for i in range(10):
             digit = digits_input[i]
             dataset.append(digit)
-            expected = [[1 if i == j else 0 for j in range(10)]]
+            expected = [1 if i == j else 0 for j in range(10)]
             results.append(expected)
 
         training_input, training_output = shuffle_data(dataset, results)
@@ -90,9 +94,11 @@ def MultilayerPerceptronMnistRecognizeNumber(probability, hidden_layer_nodes: in
 
         test_output = results
 
-        nn.train_on_dataset(training_input, training_output, iterations, epochs, learning_rate)
+        training_errors = nn.train_on_dataset(training_input, training_output, iterations, epochs, learning_rate)
 
         for i in range(len(test_output)):
             print(test_input[i].reshape(7, 5))
             print(f"expected; {test_output[i]}, got: {nn.feed_forward(test_input[i])}")
 
+        testing_error = nn.get_error_on_dataset(test_input, test_output)
+        return avg(training_errors[-1]), avg(testing_error)
