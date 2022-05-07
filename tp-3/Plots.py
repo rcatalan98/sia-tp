ec8765f9aa7b4072c3f85140770d0d09f35dc820
lgalende <lgalende@itba.edu.ar>
2022-05-07 10:40:47 -0300

    NeuralNetwork.py threshold removed, plots divided into two

--- tp-3/Plots.py
+++ tp-3/Plots.py
@@ -1,21 +1,51 @@
 from matplotlib import pyplot as plt, colors as mcolors
 
 
-def iterations_vs_error(iterations, results, epochs, title: str = None):
+def iterations_vs_error_training(iterations, results, epochs, title: str = None):
     css4, xkcd = get_colors(len(iterations))
 
     epochs_x = [i for i in range(1, epochs+1)]
     for i in range(len(iterations)):
-        plt.plot(epochs_x, results[i]["training_error"], label=f"{iterations[i]} iter training", color=css4[i])
-        plt.plot(epochs_x, results[i]["testing_error"], label=f"{iterations[i]} iter testing", linestyle="dotted", color=xkcd[i])
+        plt.plot(epochs_x, results[i]["training_error"], label=f"{iterations[i]} iter", color=css4[i])
 
     plt.xlabel('epoch')
     plt.ylabel('error')
-    plt.title(title)
+    plt.title(f'{title} - training set')
 
     plt.legend(loc='center left', bbox_to_anchor=(1, 0.5))
     plt.tight_layout()
     plt.show()
+
+
+def iterations_vs_error_testing(iterations, results, epochs, title: str = None):
+    css4, xkcd = get_colors(len(iterations))
+
+    epochs_x = [i for i in range(1, epochs + 1)]
+    for i in range(len(iterations)):
+        plt.plot(epochs_x, results[i]["testing_error"], label=f"{iterations[i]} iter", color=xkcd[i])
+
+    plt.xlabel('epoch')
+    plt.ylabel('error')
+    plt.title(f'{title} - testing set')
+
+    plt.legend(loc='center left', bbox_to_anchor=(1, 0.5))
+    plt.tight_layout()
+    plt.show()
+
+
+# def epoch_vs_metric(results, epochs, metric_name, title: str = None):
+#     epochs_x = [i for i in range(1, epochs+1)]
+#
+#     plt.plot(epochs_x, results[0]["training_error"], label=f"")
+#     plt.plot(epochs_x, results[0]["testing_error"], label=f"")
+#
+#     plt.xlabel('epoch')
+#     plt.ylabel(metric_name)
+#     plt.title(title)
+#
+#     plt.legend(loc='center left', bbox_to_anchor=(1, 0.5))
+#     plt.tight_layout()
+#     plt.show()
 
 
 def get_colors(n):
